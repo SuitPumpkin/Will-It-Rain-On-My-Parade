b936571dab17f980620a701d1cf3f57cfae61990
BaboPC <Rodrigoalozan@gmail.com>
2025-10-05 17:53:13 -0600

    fixed the pin on the simple mode

--- frontend/src/components/WeatherMapSimple.vue
+++ frontend/src/components/WeatherMapSimple.vue
@@ -136,6 +136,15 @@
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import L from "leaflet";
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 const searchCity = ref("");
 const selectedCity = ref("");
